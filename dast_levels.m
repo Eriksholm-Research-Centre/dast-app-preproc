7ceb87be923be2c8d9ced34d73f65b6c77b2f6e8
Lars Bramsløw <49481929+labowitch@users.noreply.github.com>
2025-12-29 10:46:30 +0100

    Generates correct table and saves it

--- dast_levels.m
+++ dast_levels.m
@@ -1,6 +1,7 @@
-function slevels = dast_levels(tpnr, listnr)
+function Tout = dast_levels(tpnr, listnr)
 % function to check DAST files and measure levels
 %   input: listnr(s) as vector in range 1-11
+%   output: table with rms and peak levels
 
 % defaults shared with DAST app
 sopt = preproc_defaults;
@@ -14,37 +15,59 @@ dast_lists = readtable(csvfile);
 % make a row filter and select the list
 rf = rowfilter(dast_lists);
 sel_lists = dast_lists(rf.List == listnr, :);
-
-slevels = [];
-ix = 1;
+nrsent = height(sel_lists);
+header = {'SNR', 'Talker', 'Condition', 'Peak level', 'RMS level', 'Acoustic level'};
+%Tout = table;
+irow = 1;
+nrcond = 7;
 for snr = sopt.SNRs
     for talker = sopt.talkers
-        for icond = 1:7     % processing conditions
-            for isent = 1:height(sel_lists)
+        sumcond = zeros(1,nrcond);
+        peakcond = zeros(1,nrcond);
+        for icond = 1:nrcond     % processing conditions
+            rmslist = zeros(1,nrsent);
+            peaklist = zeros(1,nrsent);
+            for isent = 1:nrsent
                 filepath = fullfile(basepath, ['TP_' num2str(tpnr)], ['SNR_' num2str(snr)], talker{1}, ['condition_' num2str(icond)]);
-
+                if ~exist(filepath, "dir")
+                    % skip missing dirs
+                    continue;
+                end
                 filenr = sel_lists(isent, "Sentence").Sentence;
                 filename = fullfile(filepath, sprintf('%04d.wav', filenr));
 
                 if exist(filename, "file")
-                    fprintf('%s\n', filename);
+                    %fprintf('%s\n', filename);
                     info = audioinfo(filename);
                     y = audioread(filename);
                     peak = max(abs(y(:)));
+                    peaklist(isent) = peak;
                     rms = std(y(:));
-                    fprintf('\tOutput peak level %2.1f dBFS, rms level %2.1f dBFS, acoustic level %2.1f dB SPL \n', ...
-                        20*log10(peak), 20*log10(rms), 20*log10(rms) + sopt.peakSPL);
+                    rmslist(isent) = rms;
                     if peak >= 1
                         warning('File %s is clipped.',filename);
                     end
                 else
-                    warning('File %s not found, skipping it\n', filename);
+                    warning('\tFile %s not found.', filename);
                 end
-              
+
             end
-                % average across sentences in lists
-
-
+            % average across sentences in lists
+            peakcond(icond) = max(peaklist);
+            sumcond(icond) = sqrt(sum(peaklist.^2)/nrsent);
+            fprintf('SNR %d dB, Talker %s, Cond %d: Peak level %2.1f dBFS, rms level %2.1f dBFS, acoustic level %2.1f dB SPL \n', ...
+                snr, talker{1}, icond, 20*log10(peakcond(icond)), 20*log10(sumcond(icond)), 20*log10(sumcond(icond)) + sopt.peakSPL);
+            % build the vectors
+            SNR(irow) = snr; %#ok<*AGROW>
+            Talker(irow) = talker;
+            Condition(irow) = icond;
+            PeakLevel(irow) = 20*log10(peakcond(icond));
+            RMSLevel(irow) = 20*log10(sumcond(icond));
+            AcousticLevel(irow) = RMSLevel(irow) + sopt.peakSPL;
+            irow = irow+1;
         end
     end
 end
+% use columns here
+Tout = table(SNR', Talker', Condition', PeakLevel', RMSLevel', AcousticLevel', 'VariableNames',header);
+writetable(Tout, ['TP' num2str(tpnr) '_L' num2str(listnr) '_levels.csv']);
